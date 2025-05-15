b37902bd18d04568df4d62227f42da940ca6d30e
Agustin <torresagustin1603@gmail.com>
2025-05-15 17:49:10 -0300

    Initial commit with README and LICENSE

---
+++ hello.py
@@ -0,0 +1,17 @@
+#HOLA
+print ("Hola python")
+print ('Hola python')
+
+#Consultar tipo de dato
+print (type ('Hola python')) #Tipo 'str'
+print (type (5)) #Tipo 'int'
+print (type (5.2)) #Tipo 'float'
+print (type (True)) #Tipo 'bool'
+print (type (1 + 2j)) #Tipo 'complex'
+
+
+"""
+Coemntario 
+en varias lineas
+
+"""
